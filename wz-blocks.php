3e3400d9de4f6658f558eda9c8ffffb38d41a555
Ajay DSouza <me@ajaydsouza.com>
2022-01-07 18:57:43 +0000

    New settings for KB block

--- wz-blocks.php
+++ wz-blocks.php
@@ -10,7 +10,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wz-blocks
  *
- * @package           wz-block
+ * @package           wz-blocks
  */
 
 /**
@@ -24,7 +24,7 @@ function wz_register_blocks() {
 
 	// Register blocks in the format $dir => $render_callback.
 	$blocks = array(
-		'kb'     => 'render_wzkb_block_test', // Knowledge Base.
+		'kb'     => 'render_wzkb_block', // Knowledge Base.
 		'alerts' => '', // Knowledge Base alerts.
 	);
 
@@ -38,38 +38,3 @@ function wz_register_blocks() {
 }
 add_action( 'init', 'wz_register_blocks' );
 
-
-/**
- * Renders the `knowledgebase/knowledgebase` block on server.
- *
- * @since 2.0.0
- *
- * @param array $attributes The block attributes.
- *
- * @return string Returns the post content with latest posts added.
- */
-function render_wzkb_block_test( $attributes ) {
-
-	$attributes['extra_class'] = $attributes['className'];
-
-	$arguments = array_merge(
-		$attributes,
-		array(
-			'is_block' => 1,
-		)
-	);
-
-	$arguments = wp_parse_args( $attributes['other_attributes'], $arguments );
-
-	/**
-	 * Filters arguments passed to get_wzkb for the block.
-	 *
-	 * @since 2.0.0
-	 *
-	 * @param array $arguments  Knowledge Base block options array.
-	 * @param array $attributes Block attributes array.
-	 */
-	$arguments = apply_filters( 'wzkb_block_options', $arguments, $attributes );
-
-	return wzkb_knowledge( $arguments );
-}
